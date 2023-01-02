783f5f4927f1b24247509b379a015e0a8a089b70
Mohamed Alaa <116747353+MohamedAlaa36156@users.noreply.github.com>
2023-01-02 21:36:18 +0200

    Add files via upload

--- ITI_Grocery.py
+++ ITI_Grocery.py
@@ -1,3 +1,6 @@
+import time
+import csv
+from prettytable import PrettyTable
 goods={
 "list": ["apple","banana","cherry","mango"],
 "prices":[10	,	8	,	25	,	18],
@@ -5,52 +8,73 @@ goods={
 }
 customer=1
 owner=1
+count=0
+pwd='Passw0rd@123.'
 list1=[]
 list2=[]
 list3=[]
 listprice=[]
-buying_list=[]
-buying_price=[]
-buying_amount=[]
-total=0
-while(1):
+listtotal=[]
+x=1
+while(x):
+	buying_list=[]
+	buying_price=[]
+	buying_amount=[]
+	listprice=[]
+	total=0
 	print("------------ Welcome to ITI Grocery Shop ------------")
-	mode=input("(1)Customers \n(2)the Owner \n(0)Exit\n")
+	mode=input("(1)Customers \n(2)the Owner \n(0)Exit\n")		#Entering either customer mode or owner's mode(password required for owner)
 	print("-----------------------------------------------")
 	if mode=='2':
+		owner=1
 		while(owner):
-			owner=int(input("(1)Show Products \n(2)Add Products \n(3)Change Cost \n(0)Exit\n"))
-			if owner==1:
-				print("-----------------------------------------------")
-				print(goods.items())
-				print("-----------------------------------------------")
-				
-			elif owner==2:
-				print("-----------------------------------------------")
-				product=input("Enter Product Name : ")
-				product=product.lower()
-				goods["list"].append(product)
-				price=int(input("Enter the product price : "))
-				goods["prices"].append(price)
-				stock=int(input("Enter the Amount : "))
-				goods["stocks"].append(stock)
-				print("-----------------------------------------------")
-			elif owner==3:
-				print("-----------------------------------------------")
-				e=int(input("Which element u want to change its price : "))
-				price=int(input("Enter the new product's price : "))
-				p=e-1
-				goods["prices"].pop(p)
-				goods["prices"].insert(p,price)
-				print("-----------------------------------------------")
-			elif owner==0:
-				print("-----------------------------------------------")
+			password=input("Enter the password : ")				#enter password :) *hint* Passw0rd@123.
+			if (password==pwd):
+				owner=int(input("(1)Show Products \n(2)Add Products \n(3)Change Cost \n(0)Exit\n"))	
+				if owner==1:
+					print("-----------------------------------------------")
+					print(goods.items())						#show a list of products being sold 
+					print("-----------------------------------------------")
+					
+				elif owner==2:
+					print("-----------------------------------------------")
+					product=input("Enter Product Name : ")			#to add new products
+					product=product.lower()
+					goods["list"].append(product)
+					price=int(input("Enter the product price : "))
+					goods["prices"].append(price)
+					stock=int(input("Enter the Amount : "))
+					goods["stocks"].append(stock)
+					print("-----------------------------------------------")
+				elif owner==3:
+					print("-----------------------------------------------")
+					e=int(input("Which element u want to change its price : "))			#Change the cost value of a current product already entered.
+					price=int(input("Enter the new product's price : "))
+					p=e-1
+					goods["prices"].pop(p)
+					goods["prices"].insert(p,price)
+					print("-----------------------------------------------")
+				elif owner==0:															#Exit of owner's mode to main menu.
+					print("-----------------------------------------------")
+					owner=0
+					print("Exiting Owner's Mode")
+					print("-----------------------------------------------")
+					break;
+				else:
+					print("-----------------------------------------------")			#Wrong Input m8 check ur self again.
+					print("Wrong Entry , Try again.")
+					print("-----------------------------------------------")
+			elif(count==3):																#BYE MASTER!!
+				owner=0
 				break;
 			else:
-				print("-----------------------------------------------")
-				print("Wrong Entry , Try again.")
-				print("-----------------------------------------------")
+				print("invalid Password.")												#1)Wrong password buddy ;) can't let you pass
+				count+=1																#2)you good mate? dont u remember me?
+				if (count==3):																	
+					count=0																#3)since i am not important to u anymore... i am breaking up with you.
+					break;																
 	elif mode=='1':
+		customer=1
 		while(customer):
 			customer=int(input("(1)Buy\n(2)Show Buying list\n(3)Print Bill\n(0)Exit\n"))
 			if customer==1:
@@ -82,15 +106,44 @@ while(1):
 				print("-----------------------------------------------")
 			elif customer==3:
 				print("-----------------------------------------------")
+				table=PrettyTable()
 				print("The list of items have been bought : ",buying_list)
 				print("Price of each item",listprice)
 				print("The amounts of items in KGs : ",buying_amount)
-				print("The Total Bill : ",total)			
+				table.add_column('List of Items',buying_list)
+				table.add_column('Price',listprice)
+				table.add_column('Amount',buying_amount)
+				print(table)
+				print("The Total Bill : ",total)
+
+				listtotal.append(total)
+				
+				d = {"Items": buying_list, 
+				"Item Price":listprice, 
+				"Amount in KGs": buying_amount,
+				}
+				
+			
+				with open("customer.csv", "w") as outfile:
+    
+			
+					writerfile = csv.writer(outfile)
+      
+			
+					writerfile.writerow(d.keys())
+      
+			
+					writerfile.writerows(zip(*d.values()))
+				t = time.localtime()
+				current_time = time.strftime("%H:%M:%S", t)
+				print(current_time)
+				print("-----------------------------------------------")
 			elif customer==0:
+				customer=0
 				break;
 			else:
 				print("Wrong Entry,Try again.")
 	elif (mode=='0'):
-		break;
+		x=0
 	else:
 		print("Wrong Entry try again.")
